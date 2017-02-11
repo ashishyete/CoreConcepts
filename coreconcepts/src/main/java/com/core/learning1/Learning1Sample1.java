7952bae1784bea6c9ff5987fc9bae120ae4416e9
Ashish <Ashish@Ashish-PC.Home>
2017-02-11 01:16:20 -0700

    init commit...

---
+++ coreconcepts/src/main/java/com/core/learning1/Learning1Sample1.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.core.learning1;
+
+/**
+ * @author Ashish
+ *
+ */
+public class Learning1Sample1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("Learning1Sample1");	
+		
+		
+	}
+
+}
